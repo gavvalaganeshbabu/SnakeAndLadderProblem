f863bfbfe2ec37378551c76eefcf42d67c7d6524
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-01-28 21:38:05 +0530

    Added Code TO UC3

--- Program.cs
+++ Program.cs
@@ -7,6 +7,33 @@ namespace SnakeAndLadderProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Hi Welcome to Snake And Ladder Game :");
+            SnakeLadderGameStart();
+        }
+        public static void SnakeLadderGameStart()
+        {
+            Console.WriteLine("Please Enter your Name  :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please Roll The Die :");
+            int StartPosition = 0;
+            int PlayerPosition = 0;
+            int gotLadderPosition = 0;
+            int ByteSnakePosition = 0;
+            Random rnd = new Random();
+            int DieNumber = rnd.Next(0, 7);
+            Console.WriteLine(DieNumber + " is your die number ");
+            if (DieNumber == 0)
+            {
+                PlayerPosition += DieNumber;
+                Console.WriteLine("Your Position At " + PlayerPosition);
+            }
+            else if (DieNumber == 2)
+            {
+                Console.WriteLine("Snake Bytes You Comes to Starting Position " + PlayerPosition);
+            }
+            else {
+                Console.WriteLine("You Got ladder position at " + PlayerPosition + DieNumber);
+            }
+
         }
     }
 }
