c5dfb8a85d066f1bfb285d09e3b8ac38d05f4485
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-01-28 21:15:25 +0530

    Added to UC2

--- Program.cs
+++ Program.cs
@@ -7,13 +7,15 @@ namespace SnakeAndLadderProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Hi Welcome to Snake And Ladder Game :");
+            SnakeLadderGameStart();
+        }
+        public static void SnakeLadderGameStart() {
             Console.WriteLine("Please Enter your Name  :");
             string name = Console.ReadLine();
             Console.WriteLine("Please Roll The Die :");
-            Random rnd= new Random();
-            int DieNumber=rnd.Next(0,7);
+            Random rnd = new Random();
+            int DieNumber = rnd.Next(0, 7);
             Console.WriteLine(DieNumber + " is your die number ");
-
 
         }
     }
