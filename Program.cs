ecbdbe2a845413979fd2a500233e938c446b4508
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-01-28 23:38:11 +0530

    Not Getting Logic

--- Program.cs
+++ Program.cs
@@ -7,6 +7,35 @@ namespace SnakeAndLadderProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Hi Welcome to Snake And Ladder Game :");
+            SnakeLadderGameStart();
+        }
+        public static void SnakeLadderGameStart()
+        {
+            Console.WriteLine("Please Enter your Name  :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please Roll The Die :");
+            int PlayerPosition = 0;
+            Random rnd = new Random();
+            while (PlayerPosition <= 100)
+            {
+                int DieNumber = rnd.Next(0, 7);
+                if (DieNumber == 0)
+                {
+                    
+                    PlayerPosition += DieNumber;
+                }
+                //If Die gives 2 and playerPosition goes to every below mentioned intervals there would be a Snake byte to player
+                else if (DieNumber == 2 && PlayerPosition == 12 || PlayerPosition == 22 || PlayerPosition == 34 || PlayerPosition == 44 || PlayerPosition == 74 || PlayerPosition == 84 || PlayerPosition == 94)
+                {                    
+                    PlayerPosition -= DieNumber;
+                }
+                else
+                {                   
+                    PlayerPosition += DieNumber;
+                }              
+            }
+            Console.WriteLine("Congratulation You Won The Game ");
+            Console.WriteLine(PlayerPosition);
         }
     }
 }
