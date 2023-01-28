321cf7a5a3ed0ae1e1123e5d579717c8909b3f7c
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-01-28 21:03:41 +0530

    Added code to UC1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace SnakeAndLadderProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Snake And Ladder Game :");
+            SnakeLadderStart();
+        }
+        static void SnakeLadderStart() {
+            Console.WriteLine("Enter Your Name :");
+            string name = Console.ReadLine();
+            int StartPosition = 0;
+            Console.WriteLine("Hi "+name + " your Game is Started at " + StartPosition);
         }
     }
 }
